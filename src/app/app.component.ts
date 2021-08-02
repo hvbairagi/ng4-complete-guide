42efe179020af5c5bc57182998623ddf05ef623a
hvbairagi <hvbairagi@gmail.com>
2021-08-02 12:24:50 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public linkToBeShown = '';
-
-  setLink(link) {
-    this.linkToBeShown = link;
-  }
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }

85e35bbccb12298b9f0c09ac31f15bac9bbddfc3
hvbairagi <hvbairagi@gmail.com>
2021-08-07 11:53:02 +0530

    dropdown menu directive challenge

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -7,16 +7,9 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-detail.component.css'],
 })
 export class RecipeDetailComponent implements OnInit {
-  constructor() {}
-
   @Input('recipe') recipe: Recipe;
 
-  ngOnInit() {
-    this.recipe = {
-      name: 'N/A',
-      description: 'N/A',
-      imagePath:
-        'https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg',
-    };
-  }
+  constructor() {}
+
+  ngOnInit() {}
 }

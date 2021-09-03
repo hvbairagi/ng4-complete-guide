c4632eb4b1e84fe7ec1981b6f30947fe9060f79a
hvbairagi <hvbairagi@gmail.com>
2021-09-03 10:46:05 +0530

    Completed Section 12

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public linkToBeShown = '';
-
-  setLink(link) {
-    this.linkToBeShown = link;
-  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -24,6 +24,10 @@ export class RecipeService {
 
   constructor(private shoppingListService: ShoppingListService) {}
 
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+
   getRecipes() {
     return this.recipes.slice();
   }

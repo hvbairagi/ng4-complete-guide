8240c70ba64b879994cbd83d77f717d39e37238d
hvbairagi <hvbairagi@gmail.com>
2021-09-21 12:00:01 +0530

    Completed Section 16

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,21 +1,24 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
+
   private recipes: Recipe[] = [
     new Recipe(
-      'A Test Recipe',
-      'This is a test',
+      'Pasta',
+      'Amazing',
       'https://multiplyillustration.com/img/9487ec7398d81017053c85ef20a9fe57.jpg',
       [new Ingredient('Rajma', 1), new Ingredient('Chana', 1)]
     ),
     new Recipe(
-      'A Test Recipe',
-      'This is a test',
-      'https://multiplyillustration.com/img/9487ec7398d81017053c85ef20a9fe57.jpg',
+      'Veg Burger',
+      'Delicious',
+      'https://www.brit.co/media-library/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpbWFnZSI6Imh0dHBzOi8vYXNzZXRzLnJibC5tcy8yMTIxMDkxMy9vcmlnaW4uanBnIiwiZXhwaXJlc19hdCI6MTY0NTk2Njg4NX0.IRD_l12EqBNtLL_aVSvWbVJ6rx-fbtLbkpqj434TIkY/image.jpg?width=600&quality=85',
       [new Ingredient('French Fries', 20), new Ingredient('Bread', 10)]
     ),
   ];
@@ -32,5 +35,20 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice());
   }
 }

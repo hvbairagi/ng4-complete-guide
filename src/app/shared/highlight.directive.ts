45574a616e5ee754ed1ba4262311766c35f3d4b3
Harsh Vardhan Bairagi <54072364+hvbairagi@users.noreply.github.com>
2024-02-23 10:22:58 +0530

    Completed Lesson 354

--- src/app/shared/highlight.directive.ts
+++ src/app/shared/highlight.directive.ts
@@ -1,8 +1,9 @@
 import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
+  standalone: true,
   selector: '[appHighlight]',
-})
+  })
 export class HighlightDirective {
   constructor(private element: ElementRef) {
     this.element.nativeElement.style.backgroundColor = '#5f5aee';
